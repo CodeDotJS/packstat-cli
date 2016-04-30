5c9517679abbcaf3ac92bad134f0a2a9ddca5b1c
CodeDotJS <rushforlinux@gmail.com>
2016-05-01 02:40:25 +0530

    done | exec

---
+++ cli.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const packStat = require('packstat');
+
+const colors = require('colors');
+
+const argv = require('yargs')
+
+    .usage(colors.cyan.bold('\nUsage: $0 -u [user.name]'))
+
+    .demand(['u'])
+
+    .describe('u', 'Node module name')
+
+    .argv;
+
+packStat(argv.u).then(user => {
+	const inf = [];
+
+	const packageRow = (prefix, key) => {
+		if (user[key]) {
+			inf.push(`${prefix}: ${user[key]}`);
+		}
+	};
+
+	console.log('\n');
+
+	packageRow(' ❱ Last Day   ', 'lastDay');
+
+	packageRow(' ❱ Last Week  ', 'lastWeek');
+
+	packageRow(' ❱ Last Month ', 'lastMonth');
+
+	console.log(inf.join('\n'));
+
+	console.log('\n');
+});
