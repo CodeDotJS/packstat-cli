9dd790d284b01c81bcbd169be7b3eb1c3c76bf19
Rishi Giri <rushforlinux@gmail.com>
2017-05-14 20:11:44 +0530

    update tests

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 import childProcess from 'child_process';
 import test from 'ava';
-import pify from 'pify';
 
-test(async t => {
-	const stdout = await pify(childProcess.execFile)('./cli.js', ['-u express'], {cwd: __dirname});
-	t.is(stdout.trim().split('\n')[0], '');
+test.cb(t => {
+	const cp = childProcess.spawn('./cli.js', ['express'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
 });
+
